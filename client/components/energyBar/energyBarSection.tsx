aa211165739a882a637805499605e10e2a0bfcc5
Güven Fazlı <guven.fazli@gmail.com>
2025-07-23 02:24:57 +0300

    feat: state for energy rate is added. Dynamic energy changing is completed

--- client/components/energyBar/energyBarSection.tsx
+++ client/components/energyBar/energyBarSection.tsx
@@ -1,10 +1,16 @@
+"use client"
 import TimerSection from "./timerSection"
 import ProgressBar from "./progressBar"
+import { useState } from "react"
+
 export default function EnergyBarSection() {
+
+  const [energy, setEnergy] = useState(75)
+
   return (
     <>
       <TimerSection />
-      <ProgressBar />
+      <ProgressBar energy={energy} />
     </>
   )
 }
--- client/components/energyBar/progressBar.tsx
+++ client/components/energyBar/progressBar.tsx
@@ -1,13 +1,17 @@
-export default function ProgressBar() {
+interface ComponentProps {
+  energy: number
+}
+
+export default function ProgressBar({ energy }: ComponentProps) {
   return (
     <div className="relative w-full">
       <div className="flex relative bg-[#1a1a1a] w-full h-[26px] shadow-[0_0_6px_2px_#F8B0DC] rounded-full">
         <div className="absolute top-1 h-[17px] rounded-full" style={{ width: `${87}%` }}>
-          <span className={`absolute h-[17px] rounded-full`} style={{ width: `${100}%`, background: `#EE39A8`, boxShadow: `inset 0 0 4px #FFFFFF` }} />
+          <span className={`absolute h-[17px] rounded-full`} style={{ width: `${energy}%`, background: `#EE39A8`, boxShadow: `inset 0 0 5px #FFFFFF` }} />
         </div>
       </div>
       <p className="absolute right-2 top-1/2 -translate-y-1/2 text-[#EE39A8] font-semibold text-sm pointer-events-none">
-        %100
+        %{energy}
       </p>
     </div>
   )
