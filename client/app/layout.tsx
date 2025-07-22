dc559526ed6d15f3b91390f9062f1edc40e0b650
Güven Fazlı <guven.fazli@gmail.com>
2025-07-23 01:25:26 +0300

    style: background gradient is done

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen w-full bg-gradient-to-b from-[#111118] to-[#353345] text-white`}
       >
         {children}
       </body>
