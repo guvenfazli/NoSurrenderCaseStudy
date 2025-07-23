6a539652f355dbe4e958829f94f7a3e0b9f59208
Güven Fazlı <guven.fazli@gmail.com>
2025-07-23 03:16:08 +0300

    refactor: reorganize HTML layout and improve structure | grid created

--- client/components/cardSection/cardSection.tsx
+++ client/components/cardSection/cardSection.tsx
@@ -1,9 +1,10 @@
 import NavBar from "../navBar/navBar"
+import ItemMenu from "./itemMenu"
 export default function CardSection() {
   return (
-    <>
+    <section className="flex flex-col justify-start items-start gap-5">
       <NavBar />
-
-    </>
+      <ItemMenu />
+    </section>
   )
 }
