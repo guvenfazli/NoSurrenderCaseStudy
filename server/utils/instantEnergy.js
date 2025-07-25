89a7a3539c12b39a113ab9677c2286449c038654
Güven Fazlı <guven.fazli@gmail.com>
2025-07-25 04:42:30 +0300

    fix: Wrong energy form from instant energy function is done

--- server/utils/instantEnergy.js
+++ server/utils/instantEnergy.js
@@ -1,24 +1,28 @@
 const Energy = require('../models/energy')
 const redisClient = require('../utils/redis')
+const dayjs = require('dayjs')
 
 async function energyCheck(requiredEnergy) {
   try {
     const cachedValue = await redisClient.get('energy/:userId')
 
     if (cachedValue) {
-      const cachedEnergy = +cachedValue // Gets the cached value
-
+      const cache = JSON.parse(cachedValue) // Gets the cached value
+      const cachedEnergy = cache.energy
       if (cachedEnergy < requiredEnergy) {
         const error = new Error()
         error.message = "Yeterli enerjin yok!"
         throw error
       }
+      const now = dayjs();
+      const lastUpdate = dayjs.unix(now);
 
       const updatedEnergy = cachedEnergy - requiredEnergy
-      await redisClient.set(`energy/:userId`, updatedEnergy, { expiration: { type: 'EX', value: 5 * 60 } }) // Updates the cache
+      await redisClient.set(`energy/:userId`, JSON.stringify({ energy: updatedEnergy, lastUpdateStamp: lastUpdate }), { expiration: { type: 'EX', value: 5 * 60 } }) // Updates the cache
       return updatedEnergy
 
     } else {
+
       const energy = await Energy.find({})
 
       if (energy[0].energy < requiredEnergy) {
@@ -27,8 +31,11 @@ async function energyCheck(requiredEnergy) {
         throw error
       }
 
+      const now = dayjs();
+      const lastUpdate = dayjs.unix(now);
+
       const updatedEnergy = energy[0].energy - requiredEnergy
-      await redisClient.set(`energy/:userId`, updatedEnergy, { expiration: { type: 'EX', value: 5 * 60 } })
+      await redisClient.set(`energy/:userId`, JSON.stringify({ energy: updatedEnergy, lastUpdateStamp: lastUpdate }), { expiration: { type: 'EX', value: 5 * 60 } })
       return updatedEnergy
     }
   } catch (err) {
